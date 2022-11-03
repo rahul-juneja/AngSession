eb03685f96a483d390443ad4d80ac29993e8289f
DeeRanjan <Dibya@8456>
2022-11-03 16:53:52 +0530

    New Component

---
+++ src/app/colab/dibya/dibya.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dibya',
+  templateUrl: './dibya.component.html',
+  styleUrls: ['./dibya.component.scss']
+})
+export class DibyaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
