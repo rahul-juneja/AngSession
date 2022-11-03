62beb43786428aba7728892c7b907fc930ba7fe3
Rahul Juneja <rahul.juneja@orionesolutions.in>
2022-11-03 20:31:23 +0530

    Gauri Component Added

--- src/app/colab/colab.module.ts
+++ src/app/colab/colab.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RahulComponent } from './rahul/rahul.component';
+import { ChikuComponent } from './chiku/chiku.component';
 import { DibyaComponent } from './dibya/dibya.component';
 
 
 
 @NgModule({
   declarations: [
-    RahulComponent, 
+    RahulComponent,
+    ChikuComponent,
     DibyaComponent
   ],
   imports: [
