465842f1d2e53669548559ecedce9a1cbb9834ef
gourichiku <gouri.sankar@orionesolutions.in>
2022-11-09 15:35:36 +0530

    Added Profile Settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart/cart.component';
+import { SettingsComponent } from './profile-section/settings/settings.component';
 import { HeaderComponent } from './webpage/header/header.component';
 
 const routes: Routes = [
   {path:'header', component:HeaderComponent},
-  {path:'cart', component:CartComponent}
+  {path:'cart', component:CartComponent},
+  {path:"setting",component:SettingsComponent}
 ];
 
 @NgModule({
