33919cd3fbf04b89535438e72340cc8cdf0e2df5
gourichiku <gouri.sankar@orionesolutions.in>
2022-11-10 18:57:13 +0530

    Profile Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,11 +22,7 @@ import { HeaderComponent } from './webpage/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ColabModule,
-    UsersModule,
-    ProductModule,
-    CartModule
+    AppRoutingModule
   ],
   providers: [HeaderComponent],
   bootstrap: [AppComponent]
