2d61732e691b3afa3bfb67b235fd7d1b563d3386
Rahul Juneja <rahul.juneja@orionesolutions.in>
2022-11-10 14:32:21 +0530

    Added Angular Functionality to Mens Page

--- src/app/product/women/women.component.ts
+++ src/app/product/women/women.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-women',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WomenComponent implements OnInit {
  isLogin = false
-  constructor() { }
+ data:any
+  constructor(private products: ProductsService) { }
 
   ngOnInit(): void {
+    this.products.showFunc().subscribe((data:any)=>{
+      data.map((val:any)=>{
+        this.data = data
+        console.log(data)
+        
+        
+      })
+    })
+    console.log(this.data)
   }
 
 }
